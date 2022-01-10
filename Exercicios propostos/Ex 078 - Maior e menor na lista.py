6bdabca8851c1de955513e9b42afc9a5bf622772
luks-rossato <89791115+luks-rossato@users.noreply.github.com>
2022-01-10 17:32:44 -0300

    Mais exercicios

--- Exercicios propostos/Ex 078 - Maior e menor na lista.py
+++ Exercicios propostos/Ex 078 - Maior e menor na lista.py
@@ -1,4 +1,24 @@
 lista = list()
+maior = 0
+menor = 0
 for i in range(0,5):
     lista.append(int(input('Digite um valor:')))
-vmaior = lista.
+    if i == 0:
+        maior = lista[i]
+        menor = lista[i]
+    else:
+        if maior < lista[i]:
+            maior = lista[i]
+        if menor > lista[i]:
+            menor = lista[i]
+
+print(f'O maior valor encontrado foi o {maior} na posição ', end=' ')
+for v, p in enumerate(lista):
+    if p == maior:
+        print(f'{v}\n')
+print(f'O menor valor encontrado foi o {menor} na posição ', end=' ')
+for v, p in enumerate(lista):
+    if p == menor:
+        print(f'{v}\n')
+
+
